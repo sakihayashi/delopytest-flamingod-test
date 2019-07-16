d168a435daee5576015b839bc8e117933c2237e2
Saki Hayashi <saki.hayashi@codeimmersive.com>
2019-07-16 15:24:23 -0400

    add recaptcha api key

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -160,7 +160,10 @@ class Form extends React.Component {
             />
             <span>Get news updates</span>
           </label>
-          <div data-netlify-recaptcha="true"></div>
+          <div
+            className="g-recaptcha"
+            data-sitekey="6LcO_60UAAAAAGjfcy0tmxdkeePZxmRyJW8weMMs"
+          />
           <input
             className="Button Form--SubmitButton"
             type="submit"
