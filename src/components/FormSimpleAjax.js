54ee556039d0b474b56b35325622215e0c4d4b56
Saki Hayashi <saki.hayashi@codeimmersive.com>
2019-07-16 14:07:10 -0400

    add submit button

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -1,4 +1,3 @@
-
 import React, { Fragment } from 'react'
 import Helmet from 'react-helmet'
 import { stringify } from 'qs'
@@ -8,7 +7,7 @@ import './Form.css'
 
 class Form extends React.Component {
   static defaultProps = {
-    name: 'hamster',
+    name: 'newHamster',
     subject: '', // optional subject of the notification email
     action: '',
     successMessage: 'Thanks for your enquiry, we will get back to you soon',
@@ -29,10 +28,7 @@ class Form extends React.Component {
     const data = serialize(form)
     this.setState({ disabled: true })
     fetch(form.action + '?' + stringify(data), {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
+      method: 'POST'
     })
       .then(res => {
         if (res.ok) {
@@ -68,7 +64,7 @@ class Form extends React.Component {
         <form
           className="Form"
           name={name}
-          method="post"
+          onSubmit={this.handleSubmit}
           data-netlify="true"
         >
           <input type="hidden" name="form-name" value={name} />
@@ -81,7 +77,7 @@ class Form extends React.Component {
                 className="Form--Input Form--InputText"
                 type="text"
                 placeholder="Firstname"
-                name="name"
+                name="firstname"
                 required
               />
               <span>Firstname</span>
@@ -123,7 +119,7 @@ class Form extends React.Component {
               className="Form--Input Form--InputText"
               type="email"
               placeholder="Email"
-              name="email"
+              name="emailAddress"
               required
             />
             <span>Email address</span>
@@ -161,7 +157,7 @@ class Form extends React.Component {
             />
             <span>Get news updates</span>
           </label>
-          
+      
           <input
             className="Button Form--SubmitButton"
             type="submit"
@@ -379,5 +375,194 @@ class Form extends React.Component {
 }
 
 export default Form
+
+*/
+
+
+/*
+------------------------
+
+latest working form 
+
+
+import React, { Fragment } from 'react'
+import Helmet from 'react-helmet'
+import { stringify } from 'qs'
+import { serialize } from 'dom-form-serializer'
+
+import './Form.css'
+
+class Form extends React.Component {
+  static defaultProps = {
+    name: 'hamster',
+    subject: '', // optional subject of the notification email
+    action: '',
+    successMessage: 'Thanks for your enquiry, we will get back to you soon',
+    errorMessage:
+      'There is a problem, your message has not been sent, please try contacting us via email'
+  }
+
+  state = {
+    alert: '',
+    disabled: false
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.disabled) return
+
+    const form = e.target
+    const data = serialize(form)
+    this.setState({ disabled: true })
+    fetch(form.action + '?' + stringify(data), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+      .then(res => {
+        if (res.ok) {
+          return res
+        } else {
+          throw new Error('Network error')
+        }
+      })
+      .then(() => {
+        form.reset()
+        this.setState({
+          alert: this.props.successMessage,
+          disabled: false
+        })
+      })
+      .catch(err => {
+        console.error(err)
+        this.setState({
+          disabled: false,
+          alert: this.props.errorMessage
+        })
+      })
+  }
+
+  render() {
+    const { name, subject, action } = this.props
+
+    return (
+      <Fragment>
+        <Helmet>
+          <script src="https://www.google.com/recaptcha/api.js" />
+        </Helmet>
+        <form
+          className="Form"
+          name={name}
+          method="post"
+          data-netlify="true"
+        >
+          <input type="hidden" name="form-name" value={name} />
+          {this.state.alert && (
+            <div className="Form--Alert">{this.state.alert}</div>
+          )}
+          <div className="Form--Group">
+            <label className="Form--Label">
+              <input
+                className="Form--Input Form--InputText"
+                type="text"
+                placeholder="Firstname"
+                name="name"
+                required
+              />
+              <span>Firstname</span>
+            </label>
+            <label className="Form--Label">
+              <input
+                className="Form--Input Form--InputText"
+                type="text"
+                placeholder="Lastname"
+                name="lastname"
+                required
+              />
+              <span>Lastname</span>
+            </label>
+          </div>
+          <fieldset>
+            <label className="Form--Label Form--Radio">
+              <input
+                className="Form--RadioInput"
+                type="radio"
+                name="gender"
+                value="male"
+                defaultChecked
+              />
+              <span>Male</span>
+            </label>
+            <label className="Form--Label Form--Radio">
+              <input
+                className="Form--RadioInput"
+                type="radio"
+                name="gender"
+                value="female"
+              />
+              <span>Female</span>
+            </label>
+          </fieldset>
+          <label className="Form--Label">
+            <input
+              className="Form--Input Form--InputText"
+              type="email"
+              placeholder="Email"
+              name="email"
+              required
+            />
+            <span>Email address</span>
+          </label>
+          <label className="Form--Label has-arrow">
+            <select
+              className="Form--Input Form--Select"
+              name="type"
+              defaultValue="Type of Enquiry"
+              required
+            >
+              <option disabled hidden>
+                Type of Enquiry
+              </option>
+              <option>Need to know more</option>
+              <option>Found a bug</option>
+              <option>Want to say hello</option>
+            </select>
+          </label>
+          <label className="Form--Label">
+            <textarea
+              className="Form--Input Form--Textarea Form--InputText"
+              placeholder="Message"
+              name="message"
+              rows="10"
+              required
+            />
+            <span>Message</span>
+          </label>
+          <label className="Form--Label Form-Checkbox">
+            <input
+              className="Form--Input Form--Textarea Form--CheckboxInput"
+              name="newsletter"
+              type="checkbox"
+            />
+            <span>Get news updates</span>
+          </label>
+          
+          <input
+            className="Button Form--SubmitButton"
+            type="submit"
+            value="Enquire"
+            disabled={this.state.disabled}
+          />
+        </form>
+      </Fragment>
+    )
+  }
+}
+
+export default Form
+
+
+
 
 */
--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -57,7 +57,7 @@ export const ContactPageTemplate = ({
         </div>
 
         <div>
-          <FormSimpleAjax name="hamster" />
+          <FormSimpleAjax name="newHamster" />
         </div>
       </div>
     </section>
