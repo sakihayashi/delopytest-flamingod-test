8aa4d8137c7b81644326e3eb7cae67626a8ccaaa
Saki Hayashi <saki.hayashi@codeimmersive.com>
2019-07-16 12:19:34 -0400

    comment out onSubmit

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -8,6 +8,189 @@ import './Form.css'
 
 class Form extends React.Component {
   static defaultProps = {
+    name: 'hamster',
+    subject: '', // optional subject of the notification email
+    action: '',
+    successMessage: 'Thanks for your enquiry, we will get back to you soon',
+    errorMessage:
+      'There is a problem, your message has not been sent, please try contacting us via email'
+  }
+
+  state = {
+    alert: '',
+    disabled: false
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.disabled) return
+
+    const form = e.target
+    const data = serialize(form)
+    this.setState({ disabled: true })
+    fetch(form.action + '?' + stringify(data), {
+      method: 'POST'
+    })
+      .then(res => {
+        if (res.ok) {
+          return res
+        } else {
+          throw new Error('Network error')
+        }
+      })
+      .then(() => {
+        form.reset()
+        this.setState({
+          alert: this.props.successMessage,
+          disabled: false
+        })
+      })
+      .catch(err => {
+        console.error(err)
+        this.setState({
+          disabled: false,
+          alert: this.props.errorMessage
+        })
+      })
+  }
+
+  render() {
+    const { name, subject, action } = this.props
+
+    return (
+      <Fragment>
+        <Helmet>
+          <script src="https://www.google.com/recaptcha/api.js" />
+        </Helmet>
+        <form
+          className="Form"
+          name={name}
+          //action={action}
+          //onSubmit={this.handleSubmit}
+          data-netlify="true"
+          netlify-recaptcha=""
+        >
+          {this.state.alert && (
+            <div className="Form--Alert">{this.state.alert}</div>
+          )}
+          <div className="Form--Group">
+            <label className="Form--Label">
+              <input
+                className="Form--Input Form--InputText"
+                type="text"
+                placeholder="Firstname"
+                name="firstname"
+                required
+              />
+              <span>Firstname</span>
+            </label>
+            <label className="Form--Label">
+              <input
+                className="Form--Input Form--InputText"
+                type="text"
+                placeholder="Lastname"
+                name="lastname"
+                required
+              />
+              <span>Lastname</span>
+            </label>
+          </div>
+          <fieldset>
+            <label className="Form--Label Form--Radio">
+              <input
+                className="Form--RadioInput"
+                type="radio"
+                name="gender"
+                value="male"
+                defaultChecked
+              />
+              <span>Male</span>
+            </label>
+            <label className="Form--Label Form--Radio">
+              <input
+                className="Form--RadioInput"
+                type="radio"
+                name="gender"
+                value="female"
+              />
+              <span>Female</span>
+            </label>
+          </fieldset>
+          <label className="Form--Label">
+            <input
+              className="Form--Input Form--InputText"
+              type="email"
+              placeholder="Email"
+              name="emailAddress"
+              required
+            />
+            <span>Email address</span>
+          </label>
+          <label className="Form--Label has-arrow">
+            <select
+              className="Form--Input Form--Select"
+              name="type"
+              defaultValue="Type of Enquiry"
+              required
+            >
+              <option disabled hidden>
+                Type of Enquiry
+              </option>
+              <option>Need to know more</option>
+              <option>Found a bug</option>
+              <option>Want to say hello</option>
+            </select>
+          </label>
+          <label className="Form--Label">
+            <textarea
+              className="Form--Input Form--Textarea Form--InputText"
+              placeholder="Message"
+              name="message"
+              rows="10"
+              required
+            />
+            <span>Message</span>
+          </label>
+          <label className="Form--Label Form-Checkbox">
+            <input
+              className="Form--Input Form--Textarea Form--CheckboxInput"
+              name="newsletter"
+              type="checkbox"
+            />
+            <span>Get news updates</span>
+          </label>
+          <div
+            className="g-recaptcha"
+            data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
+          />
+          {/* {!!subject && <input type="hidden" name="subject" value={subject} />} */}
+          <input type="hidden" name="form-name" value={name} />
+          <input
+            className="Button Form--SubmitButton"
+            type="submit"
+            value="Enquire"
+            disabled={this.state.disabled}
+          />
+        </form>
+      </Fragment>
+    )
+  }
+}
+
+export default Form
+
+/*
+
+
+import React, { Fragment } from 'react'
+import Helmet from 'react-helmet'
+import { stringify } from 'qs'
+import { serialize } from 'dom-form-serializer'
+
+import './Form.css'
+
+class Form extends React.Component {
+  static defaultProps = {
     name: 'contact',
     subject: '', // optional subject of the notification email
     action: '',
@@ -163,7 +346,7 @@ class Form extends React.Component {
             className="g-recaptcha"
             data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
           />
-          {/* {!!subject && <input type="hidden" name="subject" value={subject} />} */}
+          {!!subject && <input type="hidden" name="subject" value={subject} />} 
           <input type="hidden" name="form-name" value={name} />
           <input
             className="Button Form--SubmitButton"
@@ -178,3 +361,5 @@ class Form extends React.Component {
 }
 
 export default Form
+
+*/
